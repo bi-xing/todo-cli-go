55f63eea6db8b529983bdf895d738258ca9c4850
denzel <denzel.suprapto@gmail.com>
2026-05-31 00:27:03 +0700

    added savetask and loadtask to either read or completely replace the whole json file

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
-	"encoding.json"
-	"flag"
 	"fmt"
 	"os"
-	"time"
+  "log"
+  "encoding/json"
+  "github.com/spf13/cobra"
 )
 
-func main() {
+const datafile = "todo.json"
 
+func loadTask() []string {
+  tasks := []string
+  data, err := os.ReadFile(datafile)
+  if err == nil {
+    json.Unmarshal(data, &tasks)
+  }
+  if err != nil {
+    fmt.Println("No tasks listed.")
+  }
+  return tasks
+}
+
+func saveTask(tasks []string) {
+  data, err := json.MarshalIndent(tasks, "", "\t")
+  if err != nil {
+    log.Fatalf("Error: %v", err)
+  }
+  os.WriteFile(datafile, data, 0644)
+}
+
+func main() {
+  fmt.Printf(`
+             `)
 }
